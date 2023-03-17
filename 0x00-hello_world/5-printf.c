6c656f70155c400ba1a2c56285ea5ab0679250ac
rovenelabanga <rovenelouma2001@gmail.com>
2023-03-16 17:41:01 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that outputs a text followed by a new line using printf
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
