23646b2ae6af95fa0f7cf361be5908c3381842a0
rovenelabanga <rovenelouma2001@gmail.com>
2023-03-18 14:39:40 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This program prints single digit numbers from 0-10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
