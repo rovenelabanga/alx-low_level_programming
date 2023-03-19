eaeb710115e9995c7e22c40982b48d4053535878
rovenelabanga <rovenelouma2001@gmail.com>
2023-03-19 04:10:39 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints 1 to 9 without using char
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
