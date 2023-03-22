5f7e0157b042b199c389ffc6c5f39e275d409af2
rovenelabanga <rovenelouma2001@gmail.com>
2023-03-22 12:54:17 -0700

    5-sign

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *_is alpha - checks for alphabetic character
+ *_isalpha - checks for alphabetic character
  *@c: The character to be checked
  *Return: 1 if its c or 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if ((c >=65  && c <=90 ) || (c >=97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_sign - prints the sign of a number
+ *@n: The number to be checked
+ *Return: 1 if positive,0 if its zero and -1 if its negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	return (0);
+}
