0315568749b5275d4c8fe08857699c7d23446c1d
rovenelabanga <rovenelouma2001@gmail.com>
2023-03-22 12:25:29 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_is alpha - checks for alphabetic character
+ *@c: The character to be checked
+ *Return: 1 if its c or 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >=65  && c <=90 ) || (c >=97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
